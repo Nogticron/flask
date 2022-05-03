e6bff27536c8eda541124bf87ad6496f2f1af2c3
野口大輔 <>
2022-05-03 13:02:16 +0900

    2.2 アプリを起動する
    Blueprintの利用

---
+++ apps/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # crudパッケージからviewsをimportする
+    from apps.crud import views as crud_views
+
+    # register_blueprintを使いviewsのcrudをアプリへ登録する
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    return app
